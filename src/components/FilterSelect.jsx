8de1bc48c2fcc5462b3c5d7f48c9b0193cb3bef4
kevjinx <kevin415mei@gmail.com>
2023-02-16 08:54:41 -0800

    add grid to filterselect

--- src/components/FilterSelect.jsx
+++ src/components/FilterSelect.jsx
@@ -1,6 +1,12 @@
-import { FormControl, InputLabel, MenuItem, Select } from "@material-ui/core";
-import { useState } from "react";
-import Box from "@mui/material/Box";
+import {
+  Grid,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Box,
+} from "@mui/material";
+import { useEffect, useState } from "react";
 
 const FilterSelect = () => {
   const [filters, setFilters] = useState({
@@ -10,61 +16,84 @@ const FilterSelect = () => {
   });
 
   const handleFilterChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-
-    setFilters((prevFilters) => ({
-      ...prevFilters,
-      [name]: value,
-    }));
+    const { name, value } = event.target;
+    console.log(name, value);
+    setFilters({ ...filters, [name]: value });
   };
 
+
+
   return (
-    <Box sx={{ minWidth: 120 }}>
-      <FormControl>
-        <InputLabel id="type-label">Type</InputLabel>
-        <Select
-          labelId="type-label"
-          id="type-select"
-          value={filters.type}
-          onChange={handleFilterChange}
+    <Grid
+      container
+      spacing={2}
+      direction="row"
+      justifyContent="center"
+      alignItems="flex-start"
+    >
+      <Grid item xs >
+        <Box
+          sx={{
+            '& .MuiFormControl-root': {
+              width: '100%'
+            }
+          }}
         >
-          <MenuItem value="grass">Grass</MenuItem>
-          <MenuItem value="fire">Fire</MenuItem>
-          <MenuItem value="water">Water</MenuItem>
-        </Select>
-      </FormControl>
+          <FormControl>
+            <InputLabel id="type-label">Type</InputLabel>
+            <Select
+              autoWidth={true}
+              name="type"
+              labelId="type-label"
+              id="type-select"
+              value={filters.type}
+              onChange={handleFilterChange}
+            >
+              <MenuItem value="grass">Grass</MenuItem>
+              <MenuItem value="fire">Fire</MenuItem>
+              <MenuItem value="water">Water</MenuItem>
+            </Select>
+          </FormControl>
 
-      <FormControl>
-        <InputLabel id="weakness-label">Weaknesses</InputLabel>
-        <Select
-          labelId="weakness-label"
-          id="weakness-select"
-          value={filters.weakness}
-          onChange={handleFilterChange}
-        >
-          <MenuItem value="flying">Flying</MenuItem>
-          <MenuItem value="ground">Ground</MenuItem>
-          <MenuItem value="rock">Rock</MenuItem>
-          {/* Add more weakness options as needed */}
-        </Select>
-      </FormControl>
+        </Box>
+      </Grid>
 
-      <FormControl>
-        <InputLabel id="generation-label">Generation</InputLabel>
-        <Select
-          labelId="generation-label"
-          id="generation-select"
-          value={filters.generation}
-          onChange={handleFilterChange}
-        >
-          <MenuItem value="gen1">Gen1</MenuItem>
-          <MenuItem value="gen2">Gen2</MenuItem>
-          <MenuItem value="gen3">Gen3</MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
+      <Grid item xs>
+        <FormControl>
+          <InputLabel id="weakness-label">Weaknesses</InputLabel>
+          <Select
+            name="weakness"
+            labelId="weakness-label"
+            id="weakness-select"
+            value={filters.weakness}
+            onChange={handleFilterChange}
+          >
+            <MenuItem value="flying">Flying</MenuItem>
+            <MenuItem value="ground">Ground</MenuItem>
+            <MenuItem value="rock">Rock</MenuItem>
+            {/* Add more weakness options as needed */}
+          </Select>
+        </FormControl>
+      </Grid>
+
+      <Grid item xs>
+        <FormControl>
+          <InputLabel id="generation-label">Generation</InputLabel>
+          <Select
+            name="generation"
+            labelId="generation-label"
+            id="generation-select"
+            value={filters.generation}
+            onChange={handleFilterChange}
+          >
+            <MenuItem value="gen1">Gen1</MenuItem>
+            <MenuItem value="gen2">Gen2</MenuItem>
+            <MenuItem value="gen3">Gen3</MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
+    </Grid>
   );
-}
+};
 
 export default FilterSelect;
--- src/scene/playground/index.jsx
+++ src/scene/playground/index.jsx
@@ -2,15 +2,13 @@ import { Box } from "@mui/material";
 import PricesCandleStick from "../../components/PricesCandleStick";
 import Card from "../../components/Card";
 import Evolutions from "../../components/Evolutions";
+import FilterSelect from "../../components/FilterSelect";
 const Playground = () => {
   return (
-    <Box m="20px">
-      <Box>
-        <Card />
-      </Box>
-      <Box>
-        <Evolutions />
-      </Box>
+    <Box m="10px">
+      <Card />
+      <Evolutions />
+      <FilterSelect />
     </Box>
   );
 };
